fc2006a6c63fd316a25576e077b2ee141f820404
rogervalentim <valentim.roger33@gmail.com>
2024-09-22 23:03:41 -0300

    styles : add design to all project

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,17 @@
-import { Header } from "@/src/components/header";
+import { Header } from "@/src/components/header/header";
 
 import "./page.css";
+import { Form } from "@/src/components/form/form";
 
 export default function Home() {
   return (
     <>
       <div className="container-header">
         <Header />
+      </div>
+
+      <div className="container-form">
+        <Form />
       </div>
     </>
   );
