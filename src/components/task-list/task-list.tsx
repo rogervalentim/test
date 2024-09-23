29239e90bd44b5044dbab7011a31ff06d2baa7a8
rogervalentim <valentim.roger33@gmail.com>
2024-09-22 23:05:56 -0300

    feat: add task list

---
+++ src/components/task-list/task-list.tsx
@@ -0,0 +1,90 @@
+import Image from "next/image";
+
+import "./task-list.css";
+
+interface TaskListProps {
+  tasks: string[];
+  completedTasks: string[];
+  onDelete: (index: number, isCompletedTask?: boolean) => void;
+  onComplete: (index: number) => void;
+}
+
+export function TaskList({
+  tasks,
+  completedTasks,
+  onDelete,
+  onComplete
+}: TaskListProps) {
+  if (tasks.length === 0 && completedTasks.length === 0) {
+    return (
+      <>
+        <div className="task-empty">
+          <p>Sua lista de tarefas está vazia, adicione uma nova tarefa.</p>
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <div className="tasks-container">
+      {tasks.length > 0 &&
+        tasks.map((task, index) => (
+          <>
+            <div className="tasks-text-pending">
+              <p>Suas tarefas de hoje</p>
+            </div>
+            <div key={index} className="input">
+              <div className="checkbox">
+                <input
+                  type="checkbox"
+                  name="task"
+                  onChange={() => onComplete(index)}
+                />
+                <span>{task}</span>
+              </div>
+              <button
+                type="button"
+                className="button-trash"
+                onClick={() => onDelete(index)}
+              >
+                <Image
+                  src="/trash.svg"
+                  width={18}
+                  height={20}
+                  alt="trash icon"
+                />
+              </button>
+            </div>
+          </>
+        ))}
+
+      {completedTasks.length > 0 && (
+        <div>
+          <div className="tasks-text-finished">
+            <p>Tarefas finalizadas</p>
+          </div>
+          {completedTasks.map((task, index) => (
+            <div key={index} className="input">
+              <div className="checkbox">
+                <input type="checkbox" name="task" checked={true} readOnly />
+                <span className="completed-task-span">{task}</span>
+              </div>
+              <button
+                type="button"
+                className="button-trash"
+                onClick={() => onDelete(index, true)}
+              >
+                <Image
+                  src="/trash.svg"
+                  width={18}
+                  height={20}
+                  alt="trash icon"
+                />
+              </button>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
